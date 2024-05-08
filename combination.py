73ceff24f8f52c78a865a28988ac88e2f0fa0e58
KIHOON PARK <vpfmtlsl@naver.com>
2024-05-08 23:39:48 +0900

    Create combination.py
    implement combination without itertools

---
+++ combination.py
@@ -0,0 +1,23 @@
+def perm(length,now,remaining):
+    global n
+    if length==n:
+        print(now)
+        return
+    for i in range(len(remaining)):
+        perm(length+1,now+[remaining[i]],remaining[i+1:])
+
+
+n=int(input())
+weights=list(map(int,input().split()))
+sum_weights=sum(weights)
+result=0
+perm(0,[],weights)
+
+# input
+# 2
+# 1 2 4
+
+# output
+# [1, 2]
+# [1, 4]
+# [2, 4]
