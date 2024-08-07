de9528ce8f95256be601f348b3985139f34e56f8
KIHOON PARK <vpfmtlsl@naver.com>
2024-08-07 14:52:18 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 길찾기, Time: 164 ms, Memory: 52,708 KB -BaekjoonHub

--- SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
+++ SWEA/D4/1219. ［S／W 문제해결 기본］ 4일차 － 길찾기/［S／W 문제해결 기본］ 4일차 － 길찾기.py
@@ -1,30 +1,61 @@
-import math
+import java.io.BufferedReader;
+import java.io.InputStreamReader;import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
-times = 10
-for time in range(times):
-    a,n = map(int,input().split())
-    board1=[-1 for _ in range(100)]
-    board2=[-1 for _ in range(100)]
-    arr=list(map(int,input().split()))
-    for i in range(0,len(arr),2):
-        if board1[arr[i]]==-1:
-            board1[arr[i]]=arr[i+1]
-        else :
-            board2[arr[i]]=arr[i+1]
+public class Solution {
+	public static void main(String[] args) throws Exception {   
+       BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+       int T;
+       T=10;
+       
+       for (int test_case = 1; test_case <= T; test_case++) {
+           sb.append("#" + test_case + " ");
+            
+           // 여러분의 알고리즘 코드 작성하기
+           int len = Integer.parseInt(in.readLine().split(" ")[1]);
+           int[][] arr = new int[2][100];
+           String[] str = in.readLine().split(" ");
+           for(int i=0; i<str.length; i=i+2) {
+        	   int t1=Integer.parseInt(str[i]);
+        	   int t2=Integer.parseInt(str[i+1]);
+        	   if(arr[0][t1]==0) {
+        		   arr[0][t1]=t2;
+        	   }
+        	   else {
+        		   arr[1][t1]=t2;
+        	   }
+           }
+           
+           Stack<Integer> stk=new Stack<>();
+           stk.add(0);
+           int success=0;
+           while(stk.size()!=0) {
+        	   int tmp=stk.pop();
+        	   if(tmp==99) {
+        		   sb.append(1);
+        		   success=1;
+        		   break;
+        	   }
+        	   
+        	   if(arr[0][tmp]!=0) {
+        		   stk.add(arr[0][tmp]);
+        	   }
+        	   if(arr[1][tmp]!=0) {
+        		   stk.add(arr[1][tmp]);
+        	   }
 
-
-    ans=0
-    def dfs(pos):
-        global ans
-        if pos==-1:
-            return
-        elif pos==99:
-            ans=1
-            return
-        dfs(board1[pos])
-        dfs(board2[pos])
-
-    dfs(board1[0])
-    dfs(board2[0])
-
-    print("#",time+1, " ", ans,sep="")
+           }
+           
+           if(success==0) {
+        	   sb.append(0);
+           }
+           sb.append('\n');
+            
+       }
+        
+       System.out.println(sb);
+    }
+}
