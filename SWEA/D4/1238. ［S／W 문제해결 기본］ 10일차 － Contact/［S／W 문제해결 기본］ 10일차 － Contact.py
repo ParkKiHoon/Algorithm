9cc66a2a7090f25fd38f3497460787275636433c
KIHOON PARK <vpfmtlsl@naver.com>
2024-08-09 11:37:11 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 152 ms, Memory: 45,764 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,34 +1,68 @@
-import copy
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 
-times = 10
-for time in range(times):
-    n,m=map(int,input().split())
-    board=list(map(int,input().split()))
-    graph=[list()for _ in range(101)]
-    visited=[0 for _ in range(101)]
-    for i in range(0,n,2):
-        if board[i+1] not in graph[board[i]]:
-            graph[board[i]].append(board[i+1])
-    from collections import deque
-    queue=deque()
-    queue.append(m)
-    visited[m]=1
-    ans=0
-    while queue:
-        q=[]
-        while queue:
-            q.append(queue.popleft())
+public class Solution {
 
-        tmp=0
-        cnt=0
-        for i in q:
-            for j in graph[i]:
-                if visited[j]==0:
-                    cnt+=1
-                    visited[j]=1
-                    queue.append(j)
-                    tmp=max(tmp,j)
-        if cnt>0:
-            ans=tmp
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T;
+		T = 10;
 
-    print("#", time + 1," ", ans, sep="")
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
+			String[] split = in.readLine().split(" ");
+			int length = Integer.parseInt(split[0]);
+			int start = Integer.parseInt(split[1]);
+			ArrayList<Integer>[] arr = new ArrayList[101];
+			for (int i = 0; i < 101; i++) {
+				arr[i] = new ArrayList<>();
+			}
+
+			int[] visited = new int[101];
+			String[] split2 = in.readLine().split(" ");
+			for (int i = 0; i < split2.length; i = i + 2) {
+				arr[Integer.parseInt(split2[i])].add(Integer.parseInt(split2[i + 1]));
+			}
+
+			Deque<Integer> deque = new ArrayDeque<>();
+			deque.addLast(start);
+			while (!deque.isEmpty()) {
+				
+				ArrayList<Integer> forResult = new ArrayList<>();
+				for(int i : deque) {
+					forResult.add(i);
+				}
+				ArrayList<Integer> tmp = new ArrayList<>();
+				while (!deque.isEmpty()) {
+					int q = deque.removeFirst();
+					if (visited[q] == 0) {
+						visited[q] = 1;
+						for (int inputVal : arr[q]) {
+							if(visited[inputVal]==0)
+								tmp.add(inputVal);
+						}
+					}
+				}
+				for(int i:tmp) {
+					deque.addLast(i);
+				}
+				if(tmp.isEmpty()) {
+					int maxVal=0;
+					for(int i:forResult) {
+						maxVal=Math.max(maxVal, i);
+					}
+					sb.append(maxVal);
+				}
+			}
+
+			sb.append('\n');
+		}
+
+		System.out.println(sb);
+	}
+
+}
