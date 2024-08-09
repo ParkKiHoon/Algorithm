dbe2c8cdc737dc582915b310bbf1a6fa4de2af16
KIHOON PARK <vpfmtlsl@naver.com>
2024-08-09 10:32:16 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 173 ms, Memory: 49,564 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -1,45 +1,70 @@
-import copy
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
-times = 10
-for time in range(times):
-    n=int(input())
-    board=[]
-    start=[]
-    end=[]
-    for i in range(16):
-        tmp=input()
-        arr_tmp=[]
-        for j in range(16):
-            arr_tmp.append(int(tmp[j]))
-            if tmp[j]=="2":
-                start.append(i)
-                start.append(j)
-            elif tmp[j]=="3":
-                end.append(i)
-                end.append(j)
+public class Solution {
+    static int[] dx= {0,1,0,-1};
+    static int[] dy= {1,0,-1,0};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T;
+		T = 10;
 
-        board.append(arr_tmp)
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
+			String fil_yo_up_neunn_gap=in.readLine();
+			int[][] arr= new int[16][16];
+			int[][] visited=new int[16][16];
+			int startX = 0;
+			int startY=0;
+			int endX=0;
+			int endY=0;
+			
+			for(int i=0; i<16;i++) {
+				String[] split=in.readLine().split("");
+				for(int j=0;j<16;j++) {
+					arr[i][j]=Integer.parseInt(split[j]);
+					if(arr[i][j]==1) {
+						visited[i][j]=1;
+					}
+					if(Integer.parseInt(split[j])==2) {
+						startX=i;
+						startY=j;
+					}else if(Integer.parseInt(split[j])==3){
+						endX=i;
+						endY=j;
+					}
+				}
+			}
+			
+			Deque<int[]> queue=new ArrayDeque<>();
+			queue.push(new int[] {startX,startY});
+			int success=0;
+			while(!queue.isEmpty()) {
+				int[] tmp=queue.removeFirst();
+				int cx=tmp[0];
+				int cy=tmp[1];
+				if(arr[cx][cy]==3) {
+					success=1;
+				}
+				for(int i=0; i<4;i++) {
+					int nx=cx+dx[i];
+					int ny=cy+dy[i];
+					if(visited[nx][ny]==0) {
+						visited[nx][ny]=1;
+						queue.push(new int[] {nx,ny});
+					}
+				}
+			}
+			
+			sb.append(success);
+			sb.append('\n');
+		}
 
-    ans=0
-    visited=copy.deepcopy(board)
-    visited[start[0]][start[1]]=1
-    visited[end[0]][end[1]]=0
+		System.out.println(sb);
+	}
 
-    dx=[0,0,-1,1]
-    dy=[-1,1,0,0]
-    def dfs(x,y):
-        global end
-        global ans
-        if x==end[0] and y==end[1]:
-            ans=1
-            return
-
-        for i in range(4):
-            cx=x+dx[i]
-            cy=y+dy[i]
-            if 0<=cx<16 and 0<=cy<16 and visited[cx][cy]==0:
-                visited[cx][cy]=1
-                dfs(cx,cy)
-                visited[cx][cy]=0
-    dfs(start[0],start[1])
-    print("#", time + 1, " ", ans, sep="")
+}
