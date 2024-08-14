89f633d39d199218bb8205a09236a7acf11cfbbe
KIHOON PARK <vpfmtlsl@naver.com>
2024-08-14 15:53:35 +0900

    [D4] Title: 최소 스패닝 트리, Time: 15,338 ms, Memory: 122,152 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -1,26 +1,72 @@
-times = int(input())
-for time in range(times):
-    V,E=map(int,input().split())
-    nodes=[i for i in range(V+1)]
-    edges=[]
-    for i in range(E):
-        A,B,C=map(int,input().split())
-        edges.append([C,A,B])
-    edges.sort(key=lambda x:(x[0],x[1],x[2]))
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 
-    def find_parent(arr,x):
-        if arr[x]==x: return x
-        else: return find_parent(arr,arr[x])
+public class Solution {    
+    static int[] parent;
+    
+    static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }        
+    }
+    
+    static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if (rootA != rootB) {
+            parent[rootA] = rootB;
+        }
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(in.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#" + test_case + " ");
 
-    cnt=0
-    for C,A,B in edges:
-        a=find_parent(nodes,A)
-        b=find_parent(nodes,B)
-        if a!=b:
-            if a<b:
-                nodes[b]=a
-            else:
-                nodes[a]=b
-            cnt+=C
-
-    print("#{} {}".format(time+1,cnt))
+            String[] split = in.readLine().split(" ");
+            int V = Integer.parseInt(split[0]);
+            int E = Integer.parseInt(split[1]);
+            int[][] arr = new int[E][3];
+            parent = new int[V + 1];
+            for (int i = 0; i <= V; i++) {
+                parent[i] = i;
+            }
+            for (int i = 0; i < E; i++) {
+                String[] input = in.readLine().split(" ");
+                arr[i][0] = Integer.parseInt(input[0]);
+                arr[i][1] = Integer.parseInt(input[1]);
+                arr[i][2] = Integer.parseInt(input[2]);
+            }
+            
+            Arrays.sort(arr, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[2] - o2[2];
+                }
+            });
+            
+            long total = 0;
+            int cnt = 0;
+            for (int i = 0; i < E; i++) {
+                if (cnt == V - 1) {
+                    break;
+                }
+                if (find(arr[i][0]) != find(arr[i][1])) {
+                    union(arr[i][0], arr[i][1]);
+                    total += arr[i][2];
+                    cnt++;
+                }
+            }
+            sb.append(total);
+			sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
