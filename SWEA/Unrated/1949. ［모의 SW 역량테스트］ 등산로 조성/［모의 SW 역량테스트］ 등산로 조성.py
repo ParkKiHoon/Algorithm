a90459575eb67d61c41ec38da5c29de6c6b06c37
KIHOON PARK <vpfmtlsl@naver.com>
2024-08-18 14:19:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 914 ms, Memory: 73,964 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.py
@@ -1,41 +1,86 @@
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    n,k = map(int,input().split())
-    board = [list(map(int,input().split())) for _ in range(n)]
-    peaks = []
-    max_peak = max(map(max,board))
-    for i in range(n):
-        for j in range(n):
-            if board[i][j]==max_peak:
-                peaks.append([i,j])
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+ 
+public class Solution {
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T;
+        T = Integer.parseInt(in.readLine());
+ 
+        for (int test_case = 1; test_case <= T; test_case++) {
+            sb.append("#" + test_case + " ");
 
-
-
-    answer=[0]
-    dx=[0,0,1,-1]
-    dy=[1,-1,0,0]
-    def dfs(x,y,cnt):
-        answer[0]=max(answer[0],cnt)
-
-        for i in range(4):
-            cx=x+dx[i]
-            cy=y+dy[i]
-            if 0<=cx<n and 0<=cy<n and board[cx][cy]<board[x][y] and visited[cx][cy]==0:
-                visited[cx][cy]=1
-                dfs(cx,cy,cnt+1)
-                visited[cx][cy]=0
-
-
-    for i in range(n):
-        for j in range(n):
-            for deep in range(k):
-                deep+=1
-                if board[i][j]-deep>=0:
-                    board[i][j]-=deep
-                    for peak in peaks:
-                        visited=[[0 for _ in range(n)] for _ in range(n)]
-                        visited[peak[0]][peak[1]]=1
-                        dfs(peak[0],peak[1],0)
-                    board[i][j]+=deep
-    print("#",test_case," ",answer[0]+1,sep="")
+            String[] split = in.readLine().split(" ");
+            N=Integer.parseInt(split[0]);
+            K=Integer.parseInt(split[1]);
+            board=new int[N][N];
+            visited=new int[N][N];
+            ans=0;
+            int maxVal=0;
+            for(int i=0;i<N;i++) {
+            	String[] tmp=in.readLine().split(" ");
+            	for(int j=0;j<N;j++) {
+            		board[i][j]=Integer.parseInt(tmp[j]);
+            		maxVal=Math.max(maxVal, board[i][j]);
+            	}
+            }
+            ArrayList<int[]> pos=new ArrayList<>();
+            for(int i=0;i<N;i++) {
+            	for(int j=0;j<N;j++) {
+            		if(board[i][j]==maxVal) {
+            			pos.add(new int[] {i,j});
+            		}
+            	}
+            }
+            for(int[] i : pos) {
+            	visited[i[0]][i[1]]=1;
+            	dfs(i[0],i[1],0,board[i[0]][i[1]],1);
+            	visited[i[0]][i[1]]=0;
+            }
+            sb.append(ans+1);
+            sb.append("\n");
+ 
+        }
+ 
+        System.out.println(sb);
+    }
+    
+    static int ans;
+    static int N;
+    static int K;
+    static int[][] board;
+    static int[] dx= {1,-1,0,0};
+    static int[] dy= {0,0,1,-1};
+    static int[][] visited;
+    static void dfs(int x,int y,int sum,int before,int use) {
+    	if(sum>ans) {
+    		ans=sum;
+    	}
+    	for(int i=0; i<4; i++) {
+    		int cx=x+dx[i];
+    		int cy=y+dy[i];
+    		if(cx>=0 && cx<N && cy>=0 && cy<N && visited[cx][cy]==0) {
+    			if(board[cx][cy]<before) {
+    				visited[cx][cy]=1;
+    				dfs(cx,cy,sum+1,board[cx][cy],use);
+    				visited[cx][cy]=0;
+    			}else if(use == 1){
+    				int tmp=0;
+    				while(tmp!=K) {
+    					tmp++;
+    					if(board[cx][cy]-tmp<before){
+    						visited[cx][cy]=1;
+    						dfs(cx,cy,sum+1,board[cx][cy]-tmp,0);
+    						visited[cx][cy]=0;
+    						break;
+    					}
+    				}
+    			}
+    		}
+    	}
+    }
+}
