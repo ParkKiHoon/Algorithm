1d7855ea3d83a21129691fffdaadf5969af888ca
KIHOON PARK <vpfmtlsl@naver.com>
2024-09-05 12:49:34 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 13,241 ms, Memory: 242,712 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -1,39 +1,84 @@
-import math
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
-times = int(input())
-for time in range(times):
-    n = int(input())
-    nodes=[]
-    node=[]
-    val_x=list(map(int, input().split()))
-    val_y=list(map(int, input().split()))
-    for i in range(n):
-        nodes.append((val_x[i],val_y[i]))
-        node.append(i)
-    weight=float(input())
-    edges=[]
-    for i in range(n):
-        for j in range(n):
-            if i!=j:
-                length = (nodes[j][0]-nodes[i][0])**2+(nodes[j][1]-nodes[i][1])**2
-                edges.append((i,j,weight * length))
-    edges.sort(key=lambda x:(x[2],x[0],x[1]))
-    total=0
-    def find_parent(node,x):
-        if node[x]!=x:
-            node[x]=find_parent(node,node[x])
-        return node[x]
+public class Solution {
 
-    def union_parent(node,a,b):
-        a=find_parent(node,a)
-        b=find_parent(node,b)
-        if a<b:
-            node[b]=a
-        else:
-            node[a]=b
-    for i in edges:
-        a,b,cost=i
-        if find_parent(node,a)!= find_parent(node,b):
-            union_parent(node,a,b)
-            total+=cost
-    print("#",time+1, " ", round(total),sep="")
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T;
+		T = Integer.parseInt(in.readLine());
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			sb.append("#" + test_case + " ");
+
+			int N = Integer.parseInt(in.readLine());
+			int[] x = new int[N];
+			int[] y = new int[N];
+			double tax;
+			String[] split1 = in.readLine().split(" ");
+			String[] split2 = in.readLine().split(" ");
+			tax = Double.parseDouble(in.readLine());
+			for (int i = 0; i < N; i++) {
+				x[i] = Integer.parseInt(split1[i]);
+				y[i] = Integer.parseInt(split2[i]);
+			}
+
+			ArrayList<double[]>[] arr = new ArrayList[N];
+			for (int i = 0; i < N; i++) {
+				arr[i] = new ArrayList<>();
+			}
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (i != j) {
+						arr[i].add(new double[] { (double) j,
+								(double) (tax * (Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2))) });
+					}
+				}
+			}
+
+			PriorityQueue<double[]> queue = new PriorityQueue<>(new Comparator<double[]>() {
+				
+				@Override
+				public int compare(double[] o1, double[] o2) {
+					return Double.compare(o1[1], o2[1]);
+				}
+			});
+			
+			queue.add(new double[] {0,0});
+			
+			int[] visited=new int[N];
+			double sum=0;
+			int conn=0;
+			while(!queue.isEmpty() && conn<N) {
+				
+				double[] cur=queue.poll();
+				int node=(int)cur[0];
+				double cost=cur[1];
+				
+				if(visited[node]==1)continue;
+				
+				visited[node]=1;
+				sum+=cost;
+				conn++;
+				
+				for(double[] i:arr[node]) {
+					if(visited[(int) i[0]]==0) {
+						queue.add(new double[] {i[0],i[1]});
+					}
+				}
+				
+
+			}
+			sb.append(Math.round(sum));
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
