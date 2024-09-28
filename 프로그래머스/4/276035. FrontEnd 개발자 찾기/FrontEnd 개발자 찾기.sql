4816aeb7e16a68c8f63f5a38549d9d651a4022f4
KIHOON PARK <vpfmtlsl@naver.com>
2024-09-28 13:09:36 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,5 +1,4 @@
 select id,email,first_name,last_name
-FROM DEVELOPERS D, SKILLCODES S
-where 1=1 and (d.skill_code & s.code)>0 and s.category='front end'
-GROUP BY ID, EMAIL, FIRST_NAME, LAST_NAME
-order by id
+from developers d 
+where skill_code & (select sum(code) from skillcodes where category='front end')
+order by id 
