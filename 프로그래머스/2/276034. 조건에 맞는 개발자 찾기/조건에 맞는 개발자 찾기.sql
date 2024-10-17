0ddfed430bfb56d03b757c07cf8943e30702d931
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-18 00:17:38 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,5 +1,6 @@
-select ID,	email, FIRST_NAME,	LAST_NAME
-from DEVELOPERS
-where skill_code & (select code from skillcodes where name = 'Python')
-or skill_code & (select code from skillcodes where name = 'C#')
-order by id;
+select ID,	EMAIL,	FIRST_NAME,	LAST_NAME
+from developers
+where SKILL_CODE &(select code from skillcodes where name='Python') or 
+SKILL_CODE &(select code from skillcodes where name='C#')
+
+order by ID asc
