b14bc596c5f3b99c5b73d9c31e8ded2b28a50fa8
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-18 00:11:14 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,5 @@
-select a.item_id, a.item_name, a.rarity
-from item_info a join item_tree b
-on a.item_id=b.item_id
-
-where b.parent_item_id in (select item_id from item_info where rarity='rare')
+select a.ITEM_ID,	ITEM_NAME,	RARITY
+from ITEM_INFO a join ITEM_TREE b
+on a.item_id = b.item_id
+where b.PARENT_ITEM_ID in (select item_id from item_info where rarity='rare')
 order by a.item_id desc
