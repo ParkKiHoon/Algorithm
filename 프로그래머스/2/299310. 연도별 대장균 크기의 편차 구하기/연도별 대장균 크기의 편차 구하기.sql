0a7aa826d983489ea64f17afae6ac8ee7563ed90
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-18 20:58:53 +0900

    [level 2] Title: 연도별 대장균 크기의 편차 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
+++ 프로그래머스/2/299310. 연도별 대장균 크기의 편차 구하기/연도별 대장균 크기의 편차 구하기.sql
@@ -1,5 +1,4 @@
-select year(differentiation_date)  as year,
-(select max(size_of_colony) from ecoli_data
- where year(differentiation_date) =year) - size_of_colony as year_dev, id
- from ecoli_data
- order by year,year_dev
+select year(DIFFERENTIATION_DATE) as year, (select max(size_of_colony) 
+                                   from ecoli_data where year(DIFFERENTIATION_DATE)=year) - SIZE_OF_COLONY as YEAR_DEV, id 
+                                   from ECOLI_DATA
+                                   order by year asc , year_dev asc
