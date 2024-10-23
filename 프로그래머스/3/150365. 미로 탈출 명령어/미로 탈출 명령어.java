571b3cc4ece62a7c390cda8b5c14ea009c158bf6
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-23 17:14:08 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 6.53 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,61 +1,52 @@
-import java.util.*;
-
 class Solution {
-    static StringBuilder sb;
-    static int[] dx={1,0,0,-1};
-    static int[] dy={0,-1,1,0};
-    static String ans;
-    static String[] dstr={"d","l","r","u"};
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        x--;
-        y--;
-        r--;
-        c--;
-
-        ans="";
-        sb=new StringBuilder();
-        dfs(x,y,r,c,0,k,n,m);
-        if(ans==""){
+        String answer = "";
+        
+        int dx = r - x;
+        int dy = c - y;
+        
+        int extra = Math.abs(dx) + Math.abs(dy) - k;
+        
+        if(extra % 2 != 0) {
+            return "impossible";
+        } else if(extra > 0) {
             return "impossible";
         }
-        else{
-            return ans;
+        
+        extra = -extra;
+        
+        // d, l, r, u
+        int d = dx > 0 ? dx : 0;
+        int l = dy < 0 ? -dy : 0;
+        int rr = dy > 0 ? dy : 0;
+        int u = dx < 0 ? -dx : 0;
+        
+        x += d;
+        
+        if(x < n) {
+            int vertical = Math.min(extra/2, n-x);
+            d += vertical;
+            u += vertical;
+            extra -= vertical * 2;
         }
-    }
-    
-    static public void dfs(int cx, int cy, int r,int c, int ck,int k,int n,int m){
-        if (!ans.equals("")) return;
-    
-        int distToEnd= Math.abs(r-cx)+Math.abs(c-cy);
-        int remainK=k-ck;
-        if(distToEnd>remainK){
-            return;
+        
+        y -= l;
+        int rl = 0;
+        
+        if(extra > 0 && y > 1) {
+            int horizontal = Math.min(extra/2, y-1);
+            l += horizontal;
+            rr += horizontal;
+            extra -= horizontal * 2;
         }
-        if( (distToEnd%2 == 1 && remainK%2==0) || (distToEnd%2 == 0 && remainK%2==1) ){
-            //System.out.println(distToEnd+ " "+remainK);
-            return;
-        }
-        if(ck==k){
-            if(cx==r && cy==c){
-                String tmp=sb.toString();
-                //System.out.println(tmp);
-                if(ans.equals("")){
-                    ans=tmp;
-                }
-                else if(tmp.compareTo(ans)<0){
-                    ans=tmp;
-                }
-            }
-            return;
-        }
-        for(int i=0;i<4;i++){
-            int nx=cx+dx[i];
-            int ny=cy+dy[i];
-            if(nx>=0 && ny>=0 && nx<n && ny<m){
-                sb.append(dstr[i]);
-                dfs(nx,ny,r,c,ck+1,k,n,m);
-                sb.deleteCharAt(sb.length()-1);
-            }
-        }
+        rl = extra / 2;
+        
+        for(int i=0; i<d; i++) answer += "d";
+        for(int i=0; i<l; i++) answer += "l";
+        for(int i=0; i<rl; i++) answer += "rl";
+        for(int i=0; i<rr; i++) answer += "r";
+        for(int i=0; i<u; i++) answer += "u";
+        
+        return answer;
     }
 }
