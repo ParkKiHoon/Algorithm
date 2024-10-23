b3afeefb4c998d4b0784d76b129c55949929fc88
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-23 11:22:55 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 1536 ms, Memory: 255884 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -1,119 +1,80 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = in.readLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int M = Integer.parseInt(split[1]);
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int[][] board = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            split = in.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(split[j]);
+            }
+        }
 
-		String[] split = in.readLine().split(" ");
-		int N = Integer.parseInt(split[0]);
-		int M = Integer.parseInt(split[1]);
-		int[][] board = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			split = in.readLine().split("");
-			for (int j = 0; j < M; j++) {
-				int tmp = Integer.parseInt(split[j]);
-				if (tmp == 1) {
-					board[i][j] = -1;
-				} else {
-					board[i][j] = 0;
-				}
-			}
-		}
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        int[][] area = new int[N][M];  // 각 구역의 번호
+        int[] areaSize = new int[N * M + 1];  // 각 구역의 크기
+        int areaIndex = 1;
 
-		int[] dx = { 1, -1, 0, 0 };
-		int[] dy = { 0, 0, 1, -1 };
+        // BFS로 각 구역의 크기 계산
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == 0 && area[i][j] == 0) {
+                    Queue<int[]> queue = new ArrayDeque<>();
+                    queue.offer(new int[]{i, j});
+                    area[i][j] = areaIndex;
+                    int size = 1;
 
-		int ind = -2;
-//		int[][] conn = new int[N][M];
+                    while (!queue.isEmpty()) {
+                        int[] cur = queue.poll();
+                        int x = cur[0], y = cur[1];
+                        for (int d = 0; d < 4; d++) {
+                            int nx = x + dx[d];
+                            int ny = y + dy[d];
+                            if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 0 && area[nx][ny] == 0) {
+                                area[nx][ny] = areaIndex;
+                                queue.offer(new int[]{nx, ny});
+                                size++;
+                            }
+                        }
+                    }
+                    areaSize[areaIndex] = size;
+                    areaIndex++;
+                }
+            }
+        }
 
-		HashMap<Integer,Integer> map=new HashMap<>();
-		//ArrayList<int[]> arr = new ArrayList<>();
-		Deque<int[]> deque = new ArrayDeque<>();
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (board[i][j] == 0) {
-					int x = i;
-					int y = j;
+        // 각 벽을 부쉈을 때 이동 가능한 칸의 수 계산
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (board[i][j] == 1) {
+                    Set<Integer> connectedAreas = new HashSet<>();
+                    int moveCount = 1;  // 벽을 부쉈으므로 1칸은 기본
+                    for (int d = 0; d < 4; d++) {
+                        int nx = i + dx[d];
+                        int ny = j + dy[d];
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < M && area[nx][ny] > 0) {
+                            connectedAreas.add(area[nx][ny]);
+                        }
+                    }
+                    for (int areaId : connectedAreas) {
+                        moveCount += areaSize[areaId];
+                    }
+                    sb.append(moveCount % 10);
+                } else {
+                    sb.append(0);
+                }
+            }
+            sb.append('\n');
+        }
 
-					int cnt = 1;
-//					arr.clear();
-					deque.clear();
-					
-					//arr.add(new int[] { x, y });
-					deque.offerLast(new int[] { x, y });
-					
-					board[i][j] = ind;
-
-					while (!deque.isEmpty()) {
-						int[] queue = deque.pollFirst();
-
-						for (int d = 0; d < 4; d++) {
-							int nx = queue[0] + dx[d];
-							int ny = queue[1] + dy[d];
-							if (nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 0) {
-								board[nx][ny] = ind;
-								deque.offerLast(new int[] { nx, ny });
-								//arr.add(new int[] { nx, ny });
-								cnt++;
-							}
-						}
-					}
-
-//					for (int[] ar : arr) {
-//						board[ar[0]][ar[1]] = cnt;
-//						conn[ar[0]][ar[1]] = ind;
-//					}
-					map.put(ind, cnt);
-					ind--;
-				}
-			}
-		}
-
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(board[i]));
-//		}
-//		System.out.println();
-//		for (int i = 0; i < N; i++) {
-//			System.out.println(Arrays.toString(conn[i]));
-//		}
-		
-		int[][] ans =new int[N][M];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if(board[i][j]==-1) {
-					HashSet<Integer> set=new HashSet<>();
-					for(int d=0;d<4;d++) {
-						int nx=i+dx[d];
-						int ny=j+dy[d];
-						if(nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny]!=-1) {
-							set.add(board[nx][ny]);
-						}
-					}
-					int tmp=1;
-					for(Integer se:set) {
-						tmp+=map.get(se);
-					}
-					ans[i][j]=tmp;
-				}
-			}
-		}
-
-
-		for (int i = 0; i < N; i++) {
-			for(int j=0;j<M;j++) {
-				System.out.print(ans[i][j]%10);
-			}
-			System.out.println();
-		}
-	}
+        System.out.print(sb);
+    }
 }
