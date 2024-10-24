7fca09811705527645807fa3f60c58d8d2d06a28
KIHOON PARK <vpfmtlsl@naver.com>
2024-10-24 14:50:10 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 23.57 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 쿼드압축 후 개수 세기 - 68936 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68936) 
 
 ### 성능 요약
 
-메모리: 94.4 MB, 시간: 178.86 ms
+메모리: 113 MB, 시간: 23.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 11:03:02
+2024년 10월 24일 14:50:08
 
 ### 문제 설명
 
--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,64 +1,40 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[][] arr) {
-        int[] answer = {};
-        dfs(arr,arr.length);
-        return new int[]{zero,one};
-    }
-    static int zero=0;
-    static int one=0;
-    public void dfs(int[][] arr,int len){
+        int[] answer = {0, 0};
         
-        int cnt0=0;
-        int cnt1=0;
-        for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                if(arr[i][j]==1){
-                    cnt1++;
-                }else{
-                    cnt0++;
+        int n = arr.length / 2;
+        
+        int[][] prev = arr;
+        int[][] merge = new int[n][n];
+        
+        int[] temp = {0, 0, 0};
+        
+        while(n >= 1) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(prev[i*2][j*2] == prev[i*2][j*2+1]
+                      && prev[i*2][j*2] == prev[i*2+1][j*2]
+                      && prev[i*2][j*2] == prev[i*2+1][j*2+1]) {
+                        merge[i][j] = prev[i*2][j*2];
+                    } else {
+                        temp[prev[i*2][j*2]]++;
+                        temp[prev[i*2][j*2+1]]++;
+                        temp[prev[i*2+1][j*2]]++;
+                        temp[prev[i*2+1][j*2+1]]++;
+                        merge[i][j] = 2;
+                    }
                 }
             }
+            prev = merge;
+            n /= 2;
+            merge = new int[n][n];
         }
         
-        if(cnt1==(len*len)){
-            one+=1;
-            return;
-        }else if(cnt0==(len*len)){
-            zero+=1;
-            return;
-        }
+        temp[prev[0][0]]++;
         
-        if(len==1){
-            zero+=cnt0;
-            one+=cnt1;
-            return;
-        }
+        answer[0] = temp[0];
+        answer[1] = temp[1];
         
-        int[][] tmp1=new int[len/2][len/2];
-        int[][] tmp2=new int[len/2][len/2];
-        int[][] tmp3=new int[len/2][len/2];
-        int[][] tmp4=new int[len/2][len/2];
-        for(int i=0;i<len/2;i++){
-            for(int j=0;j<len/2;j++){
-                tmp1[i][j]=arr[i][j];
-            }
-            for(int j=len/2;j<len;j++){
-                tmp2[i][j-len/2]=arr[i][j];
-            }
-        }
-        
-        for(int i=len/2;i<len;i++){
-            for(int j=0;j<len/2;j++){
-                tmp3[i-len/2][j]=arr[i][j];
-            }
-            for(int j=len/2;j<len;j++){
-                tmp4[i-len/2][j-len/2]=arr[i][j];
-            }
-        }
-        dfs(tmp1,len/2);
-        dfs(tmp2,len/2);
-        dfs(tmp3,len/2);
-        dfs(tmp4,len/2);
+        return answer;
     }
 }
