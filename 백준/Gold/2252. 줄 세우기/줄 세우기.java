fe28be5bb69fdd1b0463499ffa638ccce63ef2c2
KIHOON PARK <vpfmtlsl@naver.com>
2024-11-04 13:19:37 +0900

    [Gold III] Title: 줄 세우기, Time: 1012 ms, Memory: 62220 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,54 +1,56 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)throws Exception {
+		
+		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] split = in.readLine().split(" ");
-		int N=Integer.parseInt(split[0]);
-		int M=Integer.parseInt(split[1]);
-		
-		
-		
-		int[] parentCnt=new int[N+1];
-		ArrayList<Integer>[] arr=new ArrayList[N+1];
-		for(int i=1;i<N+1;i++) {
-			arr[i]=new ArrayList<>();
+		String[] input = in.readLine().split(" ");
+		int N = Integer.parseInt(input[0]); 
+		int M = Integer.parseInt(input[1]);
+		ArrayList<Integer>[] arr = new ArrayList[N+1];
+		for(int n = 0; n<=N; n++) {
+			arr[n] = new ArrayList<Integer>();
 		}
+		int[] indegree = new int[N + 1];
 		
-		for(int i=0;i<M;i++) {
-			String[] tmp=in.readLine().split(" ");
-			int from=Integer.parseInt(tmp[0]);
-			int to =Integer.parseInt(tmp[1]);
-			parentCnt[to]++;
-			arr[from].add(to);
-		}
-		
-
-		Deque<Integer> deque = new ArrayDeque<>();
-		for(int i=1 ; i<N+1;i++) {
-			if(parentCnt[i]==0) {
-				deque.addLast(i);
-			}
-		}
-		
-		while(!deque.isEmpty()) {
-			int q=deque.removeFirst();
-			System.out.print(q+" ");
-			for(int i: arr[q]) {
-				parentCnt[i]--;
-				if(parentCnt[i]==0) {
-					deque.addLast(i);
-				}
-			}
+		for(int m = 0; m<M; m++) {
+			input = in.readLine().split(" ");
+			int A = Integer.parseInt(input[0]); 
+			int B = Integer.parseInt(input[1]);
 			
+			arr[A].add(B);
+			indegree[B]++;
 		}
+		
+		// 우선순위 큐 사용
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i); // 진입 차수가 0인 문제를 큐에 추가
+            }
+        }
+        
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current);
+            System.out.print(" ");
+
+            for (int next : arr[current]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.offer(next); // 진입 차수가 0이 된 문제를 큐에 추가
+                }
+            }
+        }
+		
 		
 	}
+
 }
