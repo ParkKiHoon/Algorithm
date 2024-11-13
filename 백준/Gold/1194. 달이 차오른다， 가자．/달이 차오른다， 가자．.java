0831820f28d4b6b03c2c2d8d66fb5d55945c3bf9
KIHOON PARK <vpfmtlsl@naver.com>
2024-11-13 14:19:53 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 124 ms, Memory: 16296 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.java
@@ -1,66 +1,104 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		String[] split = in.readLine().split(" ");
-		int N = Integer.parseInt(split[0]);
-		int M = Integer.parseInt(split[1]);
-		char[][] board = new char[N][M];
+	private static int N, M;
+	private static char[][] map;
+	private static boolean[][][] visited;
 
-		int startX = 0;
-		int startY = 0;
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new char[N][M];
+		visited = new boolean[N][M][64];
+
+		int sx = -1, sy = -1;
 		for (int i = 0; i < N; i++) {
-			String[] tmp = in.readLine().split("");
+			String split = in.readLine();
 			for (int j = 0; j < M; j++) {
-				board[i][j] = tmp[j].charAt(0);
-				if (board[i][j] == '0') {
-					startX = i;
-					startY = j;
+				map[i][j] = split.charAt(j);
+				if (map[i][j] == '0') { // 시작 위치
+					sx = i;
+					sy = j;
 				}
 			}
 		}
 
-		int ans=-1;
-		int[] dx = { 1, -1, 0, 0 };
-		int[] dy = { 0, 0, 1, -1 };
-		Deque<int[]> deque = new ArrayDeque<>();
-		deque.add(new int[] { startX, startY, 0, 0 });
-		int[][][] visited = new int[N][M][1 << (6+1)];
-		while (!deque.isEmpty()) {
-			int[] q = deque.pollFirst();
-			int x = q[0];
-			int y = q[1];
-			int key = q[2];
-			int cnt = q[3];
+		int ans = maze(sx, sy);
+		System.out.println(ans);
 
-			if (board[x][y] == '1') {
-				ans=cnt;
-				break;
+	}
+
+	private static int[] dx = { -1, 1, 0, 0 };
+	private static int[] dy = { 0, 0, -1, 1 };
+
+	private static int maze(int x, int y) {
+
+		Queue<int[]> queue = new ArrayDeque<int[]>();
+		queue.offer(new int[] { x, y, 0, 0 });
+		visited[x][y][0] = true;
+
+		while (!queue.isEmpty()) {
+
+			int[] curr = queue.poll();
+			x = curr[0];
+			y = curr[1];
+			int steps = curr[2];
+			int keys = curr[3];
+
+			if (map[x][y] == '1') {
+				return steps;
 			}
 
 			for (int i = 0; i < 4; i++) {
 				int nx = x + dx[i];
 				int ny = y + dy[i];
-				if (nx >= 0 && nx < N && ny >= 0 && ny < M && visited[nx][ny][key] == 0) {
-					if (65 <= board[nx][ny] && board[nx][ny] <= 70 && (key & (1 << board[nx][ny] - 64)) > 0) {
-						visited[nx][ny][key] = 1;
-						deque.add(new int[] { nx, ny, key, cnt + 1 });
-					} else if (97 <= board[nx][ny] && board[nx][ny] <= 102) {
-						visited[nx][ny][key] = 1;
-						deque.add(new int[] { nx, ny, key | (1 << (board[nx][ny] - 96)), cnt + 1 });
-					} else if (board[nx][ny] == '.' || board[nx][ny] == '0'|| board[nx][ny] == '1') {
-						visited[nx][ny][key] = 1;
-						deque.add(new int[] { nx, ny, key, cnt + 1 });
-					}
+
+				if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny][keys]) {
+					continue;
 				}
+
+				if (map[nx][ny] == '#')
+					continue;
+
+				// 열쇠인 경우
+				else if (map[nx][ny] >= 'a' && map[nx][ny] <= 'z') {
+					int newKey = keys | 1 << (map[nx][ny] - 'a');
+					if (!visited[nx][ny][newKey]) {
+						visited[nx][ny][newKey] = true;
+						queue.offer(new int[] { nx, ny, steps + 1, newKey });
+					}
+
+				}
+
+				// 문인 경우
+				else if (map[nx][ny] >= 'A' && map[nx][ny] <= 'Z') {
+					int requireKeys = 1 << (map[nx][ny] - 'A');
+					if ((requireKeys & keys) != 0) { // 1이면 해당 키가 있음
+						visited[nx][ny][keys] = true;
+						queue.offer(new int[] { nx, ny, steps + 1, keys });
+					}
+					
+				} else {
+					visited[nx][ny][keys] = true;
+					queue.offer(new int[] { nx, ny, steps + 1, keys });
+				}
+
 			}
+
 		}
-		System.out.println(ans);
+
+		return -1;
+
 	}
+
 }
